18c74bb0c6c7f8c29e4de37af574440052132296
amr <amr@gmail.coo>
2026-02-19 17:27:19 +0200

    update jenkins script

--- userdata-EC2/jenkins.sh
+++ userdata-EC2/jenkins.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 sudo apt update
- 
-sudo apt install openjdk-17-jdk -y
- 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
- 
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-/etc/apt/sources.list.d/jenkins.list > /dev/null
- 
-sudo apt-get update
- 
-sudo apt-get install jenkins -y
+sudo apt install fontconfig openjdk-21-jre -y
+
+sudo mkdir -p /etc/apt/keyrings
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt install jenkins -y
